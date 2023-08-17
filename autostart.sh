f2cd277b4aa5a953985e7d9c385648ab2ff000da
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-18 00:56:26 +0200

    autostart fix

--- autostart.sh
+++ autostart.sh
@@ -2,11 +2,10 @@
 
 picom &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-# nm-applet
+nm-applet &
 discord &
 sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
 xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
 dunst &
-# $HOME/.config/polybar/launch.sh
 
 dwm
