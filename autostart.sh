60cd8b1a6301cf555b82c0490ae6c2e05707a67d
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-17 18:16:34 +0200

    autostart setup

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+picom &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+# nm-applet
+discord &
+sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
+xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
+dunst &
+# $HOME/.config/polybar/launch.sh
+
+dwm
