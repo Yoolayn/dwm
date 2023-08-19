d8e7d24a930ae12c2d093e9c98a5583fe0ce7ab4
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-19 16:35:51 +0200

    location fix for wallpapers

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,7 @@ picom &
 # dwmblocks &
 bar.sh &
 discord &
-sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
+sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers &
 nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
 xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
 dunst &
