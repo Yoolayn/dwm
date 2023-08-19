d476f4435dbea04e09db5015f2f46a69e6e9ba15
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-19 11:28:45 +0200

    autostart nvidia settings

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,7 @@ picom &
 bar.sh &
 discord &
 sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
+nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
 xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
 dunst &
 
