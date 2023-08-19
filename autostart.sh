7de01c8f4ec206b223d60f69e33561bc39f46e1b
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-19 10:59:25 +0200

    autostart update

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,8 @@
 
 picom &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-dwmblocks &
+# dwmblocks &
+bar.sh &
 discord &
 sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
 xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
