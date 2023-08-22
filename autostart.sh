a92a3f371cf9137b75af9e8c52151db9d21368b5
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-23 00:29:18 +0200

    autostart fix

--- autostart.sh
+++ autostart.sh
@@ -3,11 +3,10 @@
 picom &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 # dwmblocks &
-bar.sh &
+~/bin/bar.sh &
 discord &
-sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers &
-nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
+# unlock-keyring &
+sleep 1 && feh --bg-fill --no-fehbg --randomize $HOME/Pictures/wallpapers/distrotube-wallpapers &
+# nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+# xrandr --output eDP-1-0 --auto --left-of HDMI-0 &
 dunst &
-
-dwm
